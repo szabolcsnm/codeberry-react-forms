73bfba0965eefdf374b37616e7232754286bdcbf
Szabolcs Derzsi <szabolcsnm@gmail.com>
2021-04-01 22:06:51 +0200

    Form component added and rendered

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
+// Default content comes with create-react-app
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import '../src/index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Form extends React.Component {
+  render () {
+      return (
+          <div className="container">
+              <div className="form-input">
+                  <h1>Form</h1>
+                  <form>
+                    <label>Name:
+                      <input type="text" id="myInput"></input>
+                    </label>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+                  </form>
+              </div>
+              <div className="form-output">
+                  <h1>Output</h1>
+              </div>
+          </div>
+      )
+  }
+}
+
+ReactDOM.render(<Form />, document.getElementById('root'));
